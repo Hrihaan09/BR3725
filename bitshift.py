16ffca4f43cda2d74380262c91416bbd706ac3d7
Hrihaan09 <157422698+Hrihaan09@users.noreply.github.com>
2025-03-07 13:44:42 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,15 @@
-def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
+#I was able to multiply two positive integers using only bit-shifting and addition by implementing a method based on the binary representation of numbers. Basically the algorithm iterates through each bit of the multiplier, shifting the multiplicand accordingly, and adding the results when the corresponding bit is set.
+
+
+def multiply(a, b):
+    result = 0
+    shift_count= 0
+    while b > 0:
+        if b & 1:  
+            result += a << shift_count  
+        a <<= 1 
+        b >>= 1 
+        shift_count += 1
+    return result
+
 
